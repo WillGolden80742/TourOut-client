79863ce4dcbc3cff2bee68456c091925267a3e54
WillGolden80742 <william80742@gmail.com>
2022-11-29 11:21:58 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/tourOut/MainActivity.java
+++ app/src/main/java/com/example/tourOut/MainActivity.java
@@ -549,16 +549,7 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-
-    private boolean isMyServiceRunning(Class<?> serviceClass) {
-        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (serviceClass.getName().equals(service.service.getClassName())) {
-                return true;
-            }
-        }
-        return false;
-    }
+    
     //DEVICE END
 
     //RUNNABLE START
